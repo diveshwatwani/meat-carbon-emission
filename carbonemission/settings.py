5123377587de5a52e25634cdd3c33f8016ec8ec1
diveshwatwani <60432327+diveshwatwani@users.noreply.github.com>
2022-03-26 11:08:40 +0530

    V7

--- carbonemission/settings.py
+++ carbonemission/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth 
+    'django.contrib.auth', 
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR,'static'),
 # ]
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "carbonemission/static",
     '/var/www/static/',
 ]
 
